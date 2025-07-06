178a8e701453b5653154e4184d02fcf6da5e0d37
RITISHAGHIMIRE <ritisha07ghimire@gmail.com>
2025-07-06 19:30:21 +0545

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -20,11 +20,7 @@ function App() {
 <Footer/>
 
 
-    {/* <main className='min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary'>
-    <Outlet/>
-    </main>
-    <footer>footer</footer>
-    <h1 className='text-3xl font-black text-amber-300 font-primary' >hello , tailwind css</h1> */}
+  
     </>
   )
 }
